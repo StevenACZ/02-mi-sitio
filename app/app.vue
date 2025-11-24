9285e0aec3d84d53cd2b6a894caa85c58710e97d
steven <scoaila@proton.me>
2025-11-24 16:28:17 -0500

    Inicio del proyecto

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, id!
+      Quaerat adipisci tenetur aperiam consectetur quasi aut voluptatibus
+      debitis quidem placeat iste fugit molestiae ducimus, id doloribus quia. A,
+      officia.
+    </p>
+  </div>
+</template>
