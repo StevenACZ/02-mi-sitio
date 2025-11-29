31abbbb6f9f146d7c4a1f736a2cd85e69225e781
steven <scoaila@proton.me>
2025-11-28 20:33:43 -0500

    Actualizar layout con nuevo header y componentes compartidos

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <MainNavigation />
-    <slot />
+  <div class="flex flex-col h-screen">
+    <!-- <MainNavigation /> -->
+    <!-- Header -->
+    <SharedHeader />
+
+    <!-- Main -->
+    <main class="container mx-auto flex-1 mt-2">
+      <slot></slot>
+    </main>
+
+    <!-- Footer -->
   </div>
 </template>
